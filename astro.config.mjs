39d53fd426ec1c43b39b15b4c89cd7039830e6be
Zong Fan <wkndnite@126.com>
2024-12-10 18:45:25 +0800

    feat(config): update favicon and remove old svg file

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,7 @@ export default defineConfig({
 	integrations: [
 		starlight({
 			title: "WkndNite Tech",
+			favicon:'/FaWikipediaW.svg',
 			social: {
 				github: "https://github.com/WkndNite",
 			},
